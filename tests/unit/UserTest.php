503d98afa590c2e3618d388fd64f3aed3c7bcf18
jaguilarodrigues <>
2020-01-25 22:32:00 -0300

    Created test for last name, full name and name are trimmed

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -4,11 +4,41 @@ class UserTest extends PHPUnit\Framework\TestCase
 {
 	public function testThatWeCanGetTheFirstName()
 	{
-		$user = new \App\Models\user;
+		$user = new \App\Models\User;
 
 		$user->setFirstName('Billy');
 
 
 		$this->assertEquals($user->getFirstName(), 'Billy');
+	}
+
+	public function testThatWeCanGetTheLastName()
+	{
+		$user = new \App\Models\User;
+
+		$user->setLastName('Garrett');
+
+
+		$this->assertEquals($user->getLastName(), 'Garrett');
+	}
+
+	public function testFullNameIsReturned()
+	{
+		$user = new \App\Models\User;
+		$user->setFirstName('Billy');
+		$user->setLastName('Garrett');
+
+		$this->assertEquals($user->getFullName(), 'Billy Garrett');
+	}
+
+	public function testFirstAndLastNameAreTrimmed()
+	{
+		$user = new \App\Models\User;
+		$user->setFirstName('Billy		');
+		$user->setLastName('		Garrett');
+
+		$this->assertEquals($user->getFirstName(), 'Billy');
+		$this->assertEquals($user->getLastName(), 'Garrett');
+		//13:06
 	}
 }
