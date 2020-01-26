22713bafc2aefca5e0f3ac378f35b01133981216
Jaqueline R Aguilar <12151003146@muz.ifsuldeminas.edu.br>
2020-01-25 23:02:37 -0300

    Created test for email address

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ class User
 
 	public $last_name;
 
+	public $email;
+
 	public function setFirstName($firstName)
 	{
 		$this->first_name = trim($firstName);
@@ -31,5 +33,23 @@ class User
 	public function getFullName()
 	{
 		return $this->first_name . ' ' . $this->last_name;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function getEmailVariables()
+	{
+		return [
+			'full_name' => $this->getFullName(),
+			'email' => $this->getEmail(),
+		];
 	}
 }
